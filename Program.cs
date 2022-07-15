db888234a0d530d7deab2b28a513c4cad0aad4b6
huseyinkarpuzcu1 <huseyinkarpuzcu371@gmail.com>
2022-07-15 12:01:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("yarrak");
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
